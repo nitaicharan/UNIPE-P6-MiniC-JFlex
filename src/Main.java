e9bae92dbde8b2a1f17d6b584102960fc69f965d
Spencer Santos <spencer.sud.santos@gmail.com>
2018-09-13 10:30:51 -0300

    Revisão realizada por spencer

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,21 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) {
+	  int contador = 0;
 
       try {
           FileReader arquivoMinic = new FileReader("src/MiniC.txt");
           ClasseJFlex analisador = new ClasseJFlex(arquivoMinic);
 
+          System.out.println("Executando a etapa de Análise Léxica\n");
+          System.out.println("------- Tokens Gerados -------\n");
           Token token = analisador.yylex();
           while (token != null){
-              System.out.println(token);
+              System.out.println("\t"+token);
               token = analisador.yylex();
+              contador++;
           }
+          System.out.println("\nTotal de tokens: "+contador+"\n\n");
       }catch (IOException e){
           e.printStackTrace();
       }
--- src/TipoToken.java
+++ src/TipoToken.java
@@ -1,5 +1,5 @@
 
-package br.com.analisorlexico;
+//package br.com.analisorlexico;
 
 public class TipoToken {
 
